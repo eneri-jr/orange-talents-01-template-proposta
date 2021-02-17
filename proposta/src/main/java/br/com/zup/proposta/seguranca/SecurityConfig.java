7b5b4411ea49dab6f4aec15ce1594ae481f093ae
eneri-jr <eneri.jr@gmail.com>
2021-02-17 16:20:51 -0300

    Login usando KeyCloak

---
+++ proposta/src/main/java/br/com/zup/proposta/seguranca/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.zup.proposta.seguranca;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        String jwkSetUri = "http://localhost:18080/auth/realms/nosso-cartao/protocol/openid-connect/certs";
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests(authorizeRequests -> authorizeRequests
+                    .anyRequest().authenticated()).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        }
+
+        @Bean
+        JwtDecoder jwtDecoder() {
+            return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+        }
+}
