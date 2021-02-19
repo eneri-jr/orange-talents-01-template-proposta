a554adf811d76ff2ca6c748702b3edee3533df1d
eneri-jr <eneri.jr@gmail.com>
2021-02-19 12:23:09 -0300

    Metricas Graficas no Prometheus

--- proposta/src/main/java/br/com/zup/proposta/seguranca/SecurityConfig.java
+++ proposta/src/main/java/br/com/zup/proposta/seguranca/SecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.zup.proposta.seguranca;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests(authorizeRequests -> authorizeRequests
+                    .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                     .anyRequest().authenticated()).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         }
 
